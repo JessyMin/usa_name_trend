e1ea8b2f8ddf3220b250120555de616fab542c2c
JessyMin <uxinsight@naver.com>
2018-08-12 01:57:19 +0900

    add joinedTable

--- server.R
+++ server.R
@@ -3,58 +3,109 @@ library(DT)
 library(dplyr)
 
 # loading local data file
-data <- read.csv("./Data/sample.csv", row.names=NULL)
-
-
+# setwd("/Users/jessymin/documents/usa_name_trend")
+data <- read.csv("./data/sample2.csv")
 
 
 server <- function(input, output){
 
-
-    # filter data
-    selectedData2 <- reactive({ 
-        req(input$year)
-        req(input$gender)
-        data <- subset(data, 
-            year == input$year & gender == input$gender)
-        })
-    
+    #Filter data with year & gender
     selectedData <- reactive({ 
         req(input$year)
         req(input$gender)
         data <- data %>% 
-            filter(year == input$year & gender == input$gender)# %>%
-            #select(rank, count, name)
+            filter(year == input$year) %>%
+            filter(gender == input$gender)    })
+
+    ###########################################################    
+    #Join 테이블
+    joinedTable <- reactive({
+        req(input$year)
+        req(input$gender)
+        y <- as.numeric(input$year)
+        
+        data1 <- sample %>% 
+            filter(year == input$year) %>%
+            filter(gender == input$gender)    
+        
+        data2 <- sample %>%
+            filter(year == y - 3) %>%
+            filter(gender == input$gender)    
+        
+        joinData <- data1 %>% 
+            left_join(data2, by = c('name'='name'))
+        rm(data1, data2)
+        
+        colnames(joinData) <- c('year','gender','rank','name','count','year_o','gender_o','rank_o','count_o')
+        
+        joinData <- joinData %>% 
+            select(c(1:5), 8) 
+        
+        joinData <- joinData %>%
+            mutate(rank_up = rank_o - rank)
+        
+        joinData <- joinData %>%
+            mutate(status = case_when(
+                joinData$rank_up > 0 ~ "▲",
+                joinData$rank_up == 0 ~ "-",
+                joinData$rank_up < 0 ~ "▽")
+            )
+        })
+    
+    
+
+    #Reactive Gender Text
+    genderText <- reactive({ 
+        if (input$gender == "F") "Female" else "Male"
     })
     
-    # Reactive name list
-    all_names <- sort(unique(selectedData()$name))
-    
-    # Reactive Gender Text
-    genderText <- reactive( if (input$gender == "F") "Female" else "Male")
-    
-    # Year Text
+    #성별/이름 Text
     output$tableInfoText <- renderText({
         paste0(genderText(), " baby names in ", input$year)
     })
     
-    # 지정한 컬럼값만 나오게 하기
+    #전체 테이블
     output$table1 <- renderDT({
         DT::datatable(
-            selectedData()[, input$show_vars, drop = FALSE], 
-            width = 300,
-            #colnames = c("Year" = year),
+            selectedData()[, input$show_vars, drop=FALSE], 
+            width=500,
             rownames=FALSE
         )
     })
     
+    #조인 테이블
+    output$table3 <- renderDT({
+        DT::datatable(
+            joinedTable()[, input$show_vars, drop=FALSE],
+            width=500,
+            rownames=FALSE
+        )
+    })
+    
+    
+    
+    
+    #선택한 이름 상세 테이블
+    selectedName <- reactive({ 
+        req(input$name)
+        data <- subset(data, name == input$name)
+    })
+    
+    #선택한 이름만 Plot 그리기
+    output$plot1 <- renderPlot({
+        d <- selectedName()
+        ggplot(data=d, aes(year, count)) + geom_line(col='steelblue') + 
+            theme_minimal()
+    })
+    
+
+    #observeEvent(input$name, {
+    #    output$table2 <- renderDT({
+    #        DT::datatable(selectedData2(), rownames=FALSE)
+    #    })    
+    #})
+    
     # 이름 고르게 하기
     #updateSelectizeInput(session, 'name', choices = data, server = TRUE)
-    
-    
-    #Test Plot
-    #output$plot <- renderPlot({
-    #    ggplot(data2(), aes(year, count, col=name)) + geom_line()
-    #})
     
 }
--- ui.R
+++ ui.R
@@ -1,5 +1,5 @@
 library(shiny)
-data <- read.csv("./Data/sample.csv")
+data <- read.csv("./Data/sample2.csv")
 
 
 ui <- fluidPage(
@@ -7,44 +7,53 @@ ui <- fluidPage(
     titlePanel("USA Name Trends"),
     
     sidebarLayout(
+        
         sidebarPanel(
             
             # Select birth year
-            selectInput("year", h4("Select birth year :"),
-                        choices = unique(data$year),
-                        selected = 1981
-            ),
+            selectInput(
+                "year", h4("Select birth year :"),
+                choices = unique(data$year),
+                selected = 1990
+            )
             
             
             # Input gender
-            radioButtons(
-                'gender', h4("Select gender :"),
+            , radioButtons(
+                "gender", h4("Select gender :"),
                 choices = list('Female' = 'F', 'Male' = 'M'),
                 selected = "F"
-            ),
+            )
             
             # Select Column
-            checkboxGroupInput(
-                'show_vars', h4("Columns in dataset to show: "),
-                choices = names(data)[-(1:2)], 
-                selected = names(data)
-            ),
+            , checkboxGroupInput(
+                "show_vars", h4("Columns in dataset to show: "),
+                choices = c('rank','name','count','rank_o','rank_up','status'),
+                selected = c('rank','name','status')
+            )
+            
+            #테스트용 텍스트인풋
+            , textInput(
+                "name", h3("Type name :"), 
+                value = ""   
+            )
+            
             
             # 이름 고르기
-            #selectizeInput(
+            #, selectizeInput(
             #    'name', h4("Name to show detail: "),
             #    choices = unique(data$name),
             #    options = list(placeholder = 'type a name')
             #)
 
             #이름 고르기
-            selectInput(
-                'name', h4("Name to show detail: "),
-                choices = unique(data$name),
+            #, selectInput(
+            #    'name', h4("Name to show detail: "),
+            #    choices = unique(data$name),
                 #placeholder = 'type a name',
-                selectize = TRUE,
-                multiple = FALSE
-            )
+            #    selectize = TRUE,
+            #    multiple = FALSE
+            #)
              #           selected = "20th Century Fox",
              #           selectize = TRUE,
              #           multiple = TRUE)
@@ -53,10 +62,15 @@ ui <- fluidPage(
         ),
         
         mainPanel(
-                p(h4(textOutput("tableInfoText"))),
-                DT::dataTableOutput("table1")
+            
+                p(h4(textOutput("tableInfoText")))
+
+                , div(DT::dataTableOutput("table3")
+                      , style = "width: 80%")
+                
+                , plotOutput("plot1")
         )
     )
-
+)
 
 
